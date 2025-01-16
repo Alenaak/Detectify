9119f36fee5aee31104883ae73b806d61fb10982
Aleena Akbar <131653812+Alenaak@users.noreply.github.com>
2025-01-16 17:08:12 +0500

    Add files via upload

---
+++ app/src/main/java/com/yolo/snapdetect/activity/MainActivity.kt
@@ -0,0 +1,211 @@
+package com.yolo.snapdetect.activity
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.yolo.snapdetect.utils.Box
+import com.yolo.snapdetect.utils.Constants.LABELS_PATH
+import com.yolo.snapdetect.utils.Constants.MODEL_PATH
+import com.yolo.snapdetect.utils.Detection
+import com.yolo.snapdetect.R
+import com.yolo.snapdetect.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity(), Detection.DetectorListener {
+    private lateinit var binding: ActivityMainBinding
+    private val isFrontCamera = false
+    // Add a flag to track the current camera lens facing
+    private var lensFacing = CameraSelector.LENS_FACING_BACK
+
+    private var preview: Preview? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+    private var detection: Detection? = null
+    private lateinit var cameraExecutor: ExecutorService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        cameraExecutor.execute {
+            detection = Detection(baseContext, MODEL_PATH, LABELS_PATH, this)
+        }
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        bindListeners()
+    }
+
+    private fun bindListeners() {
+        binding.apply {
+            isGpu.setOnCheckedChangeListener { buttonView, isChecked ->
+                cameraExecutor.submit {
+                    detection?.restart(isGpu = isChecked)
+                }
+                if (isChecked) {
+                    buttonView.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.white))
+                } else {
+                    buttonView.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.gray))
+                }
+            }
+            // Add listener for the camera toggle button
+            camera.setOnClickListener {
+                lensFacing = if (lensFacing == CameraSelector.LENS_FACING_BACK) {
+                    CameraSelector.LENS_FACING_FRONT
+                } else {
+                    CameraSelector.LENS_FACING_BACK
+                }
+                bindCameraUseCases() // Rebind the camera with the new lens facing
+            }
+
+        }
+
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            cameraProvider  = cameraProviderFuture.get()
+            bindCameraUseCases()
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun bindCameraUseCases() {
+        val cameraProvider = cameraProvider ?: throw IllegalStateException("Camera initialization failed.")
+
+        val rotation = binding.viewFinder.display.rotation
+
+        val cameraSelector = CameraSelector
+            .Builder()
+            .requireLensFacing(lensFacing)
+            .build()
+
+        preview =  Preview.Builder()
+            .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+            .setTargetRotation(rotation)
+            .build()
+
+        imageAnalyzer = ImageAnalysis.Builder()
+            .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .setTargetRotation(binding.viewFinder.display.rotation)
+            .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+            .build()
+
+        imageAnalyzer?.setAnalyzer(cameraExecutor) { imageProxy ->
+            val bitmapBuffer =
+                Bitmap.createBitmap(
+                    imageProxy.width,
+                    imageProxy.height,
+                    Bitmap.Config.ARGB_8888
+                )
+            imageProxy.use { bitmapBuffer.copyPixelsFromBuffer(imageProxy.planes[0].buffer) }
+            imageProxy.close()
+
+            val matrix = Matrix().apply {
+                postRotate(imageProxy.imageInfo.rotationDegrees.toFloat())
+
+                if (isFrontCamera) {
+                    postScale(
+                        -1f,
+                        1f,
+                        imageProxy.width.toFloat(),
+                        imageProxy.height.toFloat()
+                    )
+                }
+            }
+
+            val rotatedBitmap = Bitmap.createBitmap(
+                bitmapBuffer, 0, 0, bitmapBuffer.width, bitmapBuffer.height,
+                matrix, true
+            )
+
+            detection?.detect(rotatedBitmap)
+        }
+
+        cameraProvider.unbindAll()
+
+        try {
+            camera = cameraProvider.bindToLifecycle(
+                this,
+                cameraSelector,
+                preview,
+                imageAnalyzer
+            )
+
+            preview?.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+        } catch(exc: Exception) {
+            Log.e(TAG, "Use case binding failed", exc)
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()) {
+        if (it[Manifest.permission.CAMERA] == true) { startCamera() }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        detection?.close()
+        cameraExecutor.shutdown()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (allPermissionsGranted()){
+            startCamera()
+        } else {
+            requestPermissionLauncher.launch(REQUIRED_PERMISSIONS)
+        }
+    }
+
+    companion object {
+        private const val TAG = "Camera"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = mutableListOf (
+            Manifest.permission.CAMERA
+        ).toTypedArray()
+    }
+
+    override fun onEmptyDetect() {
+        runOnUiThread {
+            binding.overlay.clear()
+        }
+    }
+
+    override fun onDetect(boxes: List<Box>, inferenceTime: Long) {
+        runOnUiThread {
+            binding.inferenceTime.text = "${inferenceTime}ms"
+            binding.overlay.apply {
+                setResults(boxes)
+                invalidate()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/yolo/snapdetect/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.yolo.snapdetect.utils
+
+object Constants {
+    const val MODEL_PATH = "model.tflite"
+    const val LABELS_PATH = "labels.txt"
+}
